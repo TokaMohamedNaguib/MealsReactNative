0d756574b4dc887d599f64793822f3ad26d3c93f
toka naguib <toka.naguib@fekretyonline.com>
2020-02-25 11:29:25 +0200

    initial commit

---
+++ src/Screens/MealsListScreen.js
@@ -0,0 +1,23 @@
+import React from 'react' ;
+import {Text,View,StyleSheet} from 'react-native';
+
+
+
+const MealsListScreen = props => {
+
+return (
+<View>
+<Text>CategoriesScreen<Text>
+<View>
+)
+
+}
+
+const styles = StyleSheet.create({
+container:{
+  flex:1,
+}
+
+});
+
+export default CategoriesScreen;
